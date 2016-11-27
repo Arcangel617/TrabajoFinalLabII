11deed1778c606940f6775f7bd6be6c7c322bbbe
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-11-27 10:36:44 -0300

    Modifico la estructura de la clase para poder agregar dos constructores mas

--- src/main/java/app/User.java
+++ src/main/java/app/User.java
@@ -6,13 +6,20 @@ import java.util.ArrayList;
  * Created by arcangel on 23/11/16.
  */
 public class User {
-    private final long id;
+    private long id;
     private String name;
     private String email;
     private ArrayList<Calendar> calendars = new ArrayList<>();
 
+    public User(){}
+
     public User(long id, String name, String email) {
         this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(String name, String email){
         this.name = name;
         this.email = email;
     }
@@ -32,4 +39,10 @@ public class User {
     public ArrayList<Calendar> getCalendars(){
         return calendars;
     }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setCalendars(){}
 }
