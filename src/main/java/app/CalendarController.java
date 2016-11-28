2cba9982f3ad96a333e802cc8294c98bcafcca21
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-11-27 23:49:21 -0300

    Documento los métodos

--- src/main/java/app/CalendarController.java
+++ src/main/java/app/CalendarController.java
@@ -12,11 +12,20 @@ import java.util.spi.CalendarNameProvider;
 @RestController
 public class CalendarController {
 
+    /**
+     * Devuelve una coleccion con todos los calendarios.
+     * @return una coleccion de calendarios
+     */
     @RequestMapping(value = "/calendars", method = RequestMethod.GET)
     public Collection<Calendar> getAllCalendars() {
         return CalendarSource.getCalendars();
     }
 
+    /**
+     * Devuelve una coleccion con los calendarios de un usuario
+     * @param userId
+     * @return una coleccion con los calendarios del usuario
+     */
     @RequestMapping(value = "/users/{userId}/calendars", method = RequestMethod.GET)
     public Collection<Calendar> getCalendarByUser(@PathVariable("userId") Long userId) {
         Collection result = new ArrayList();
@@ -28,6 +37,12 @@ public class CalendarController {
         return result;
     }
 
+    /**
+     * Devuelve los datos del calendario que se pasa como parámentro
+     * @param userId
+     * @param calendarId
+     * @return un objeto de tipo Calendar
+     */
     @RequestMapping(value = "/users/{userId}/calendars/{calendarId}", method = RequestMethod.GET)
     public Calendar getById(@PathVariable("userId") Long userId,
                                         @PathVariable("calendarId") Long calendarId) {
@@ -37,11 +52,21 @@ public class CalendarController {
         return null;
     }
 
+    /**
+     * Agrega un calendario.
+     * @param userId
+     * @param input
+     */
     @RequestMapping(value = "/users/{userId}/calendars/add", method = RequestMethod.POST)
     public void add(@PathVariable("userId") Long userId, @RequestBody Calendar input){
         CalendarSource.addCalendar(input.getName(), userId);
     }
 
+    /**
+     * Borra un calendario.
+     * @param calendarId
+     * @param userId
+     */
     @RequestMapping(value = "/users/{userId}/calendars/{calendarId}/delete", method = RequestMethod.DELETE)
     public void delete(@PathVariable("calendarId") Long calendarId, @PathVariable("userId") Long userId){
         if (CalendarSource.getCalendar(calendarId).getOwner().getId() == userId){
@@ -49,6 +74,12 @@ public class CalendarController {
         }
     }
 
+    /**
+     * Actualiza los datos del calendario que se pasa como parámetro
+     * @param calendarId
+     * @param userId
+     * @param input
+     */
     @RequestMapping(value = "/users/{userId}/calendars/{calendarId}/update", method = RequestMethod.PUT)
     public void update(@PathVariable("calendarId") Long calendarId, @PathVariable("userId") Long userId,
                        @RequestBody Calendar input){
