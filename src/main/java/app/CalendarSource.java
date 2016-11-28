813ca081d60e38df81b43071021893a5746e3f91
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-11-27 23:49:31 -0300

    Documento los métodos

--- src/main/java/app/CalendarSource.java
+++ src/main/java/app/CalendarSource.java
@@ -23,23 +23,46 @@ public class CalendarSource {
         CALENDARS.put(calendar4.getId(), calendar4);
     }
 
+    /**
+     * Devuelve una coleccion con todos los calendarios.
+     * @return una coleccion de calendarios
+     */
     public static Collection<Calendar> getCalendars() {
         return CALENDARS.values();
     }
 
+    /**
+     * Devuelve el calendario que se pasa como parámetro
+     * @param calendarId
+     * @return un objeto de tipo Calendar
+     */
     public static Calendar getCalendar(Long calendarId){
         return CALENDARS.get(calendarId);
     }
 
+    /**
+     * Agrega un calendario.
+     * @param name
+     * @param userId
+     */
     public static void addCalendar(String name, Long userId){
         Calendar c = new Calendar(counter.incrementAndGet(), name, userId);
         CALENDARS.put(c.getId(), c);
     }
 
+    /**
+     * Borra el calendario que se pasa como parámetro.
+     * @param id
+     */
     public static void deleteCalendar(Long id){
         CALENDARS.remove(id);
     }
 
+    /**
+     * Actualiza los datos del calendario que se pasa como parámetro.
+     * @param calendarId
+     * @param name
+     */
     public static void updateCalendar(Long calendarId, String name){
         Calendar c = getCalendar(calendarId);
         c.setName(name);
