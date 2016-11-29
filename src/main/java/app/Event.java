3bb403b6aa0f249ab375c0c926ec907f00f7f78e
Arcangel Andres <artigue_arcangel@hotmail.com>
2016-11-28 21:40:55 -0300

    Implemento la logica necesaria para la creacion de los objetos

--- src/main/java/app/Event.java
+++ src/main/java/app/Event.java
@@ -4,12 +4,40 @@ package app;
  * Created by arcangel on 23/11/16.
  */
 public class Event {
-    private final long id;
-    private  String name;
+    private long id;
+    private String name;
+    private Calendar owner;
+    private String horaInicio;
+    private String horaFin;
+    private String fechaInicio;
+    private String fechaFin;
 
-    public Event(long id, String name) {
+    public Event(){}
+
+    public Event(String name){
+        this.name = name;
+    }
+
+    public Event(long id, String name, String fechaInicio, String fechaFin,
+                 String horaInicio, String horaFin) {
         this.id = id;
         this.name = name;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+
+    public Event(long id, String name, int diaInicio, int mesInicio, int anioInicio,
+                 int diaFin, int mesFin, int anioFin,
+                 int hinicio, int minicio, int sinicio,
+                 int hfin, int mfin, int sfin) {
+        this.id = id;
+        this.name = name;
+        this.fechaInicio = formatearFecha(diaInicio,mesInicio,anioInicio);
+        this.fechaFin = formatearFecha(diaFin,mesFin,anioFin);
+        this.horaInicio = formatearHora(hinicio, minicio, sinicio);
+        this.horaFin = formatearHora(hfin, mfin, sfin);
     }
 
     public long getId() {
@@ -18,5 +46,47 @@ public class Event {
 
     public String getName() {
         return name;
+    }
+
+    public Calendar getOwner(){
+        return owner;
+    }
+
+    private String formatearHora(int hora, int min, int seg){
+        StringBuilder sb = new StringBuilder();
+        if (hora < 10){
+            sb.append("0"+hora);
+        } else {
+            sb.append(hora);
+        }
+        if (min < 10){
+            sb.append(":0"+min);
+        } else {
+            sb.append(":"+min);
+        }
+        if (seg < 10){
+            sb.append(":0"+seg);
+        } else {
+            sb.append(":"+seg);
+        }
+        return sb.toString();
+
+    }
+
+    private String formatearFecha(int dia, int mes, int anio){
+        StringBuilder sb = new StringBuilder();
+        if (dia < 10){
+            sb.append("0"+dia);
+        } else {
+            sb.append(dia);
+        }
+        if (mes < 10){
+            sb.append("/0"+mes);
+        } else {
+            sb.append("/"+mes);
+        }
+        sb.append("/"+anio);
+        return sb.toString();
+
     }
 }
---
+++ src/main/java/app/EventSource.java
@@ -0,0 +1,7 @@
+package app;
+
+/**
+ * Created by arcangel on 27/11/16.
+ */
+public class EventSource {
+}
